2e85db7e831b7e9f29c9dd30436d69818ddcb0df
ExpDevStar <expdevstar000@outlook.com>
2019-07-28 11:22:48 -0500

    Added create button and console message to indicate successful creation

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import axios from "axios";
 
-Vue.config.productionTip = false
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
+
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,49 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>New Product</h1>
+    <button v-on:click="createProduct()">Create</button>
+    <h1>{{ message }}</h1>
+    <div v-for="product in products">
+      <h2>{{ product.name }}</h2>
+      <img v-bind:src="product.image_url" alt="" />
+      <p>Price: {{ product.price }}</p>
+      <p>Image: {{ product.image_url }}</p>
+      <p>Description: {{ product.description }}</p>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("/api/products").then(response => {
+      this.products = response.data;
+      console.log(this.products);
+    });
+  },
+  methods: {
+    createProduct: function() {
+      console.log("Create the product...");
+      var params = {
+        name: "THE Example title",
+        price: "Example price",
+        image_url: "url",
+        description: "Example description"
+      };
+      axios.post("/api/products", params).then(response => {
+        console.log("Great Success", response.data);
+        this.products.push(response.data);
+      });
+    }
   }
-}
+};
 </script>
